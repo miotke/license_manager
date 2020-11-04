c3610e149916e08416252f68ae6f62f7c882a7eb
Andrew Miotke <andrewmiotke@gmail.com>
2020-11-03 16:02:14 -0800

    Login and logout is now working

--- src/urls.py
+++ src/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("licenses.urls"))
+    path("", include("licenses.urls")),
+    path("login/", include("licenses.urls")),
 ]
