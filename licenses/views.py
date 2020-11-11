7ab7e4d2b62809014e1437a52f1281c63c6b581c
miotke <andrewmiotke@gmail.com>
2020-11-11 11:01:33 -0800

    Relationship between Software and License is connected and working. There is still some work to be done.

--- licenses/views.py
+++ licenses/views.py
@@ -6,6 +6,7 @@ from django.views.generic import ListView
 from django.views.generic import CreateView
 from django.views.generic import UpdateView
 from django.views.generic import DeleteView
+from .models import Software
 from .models import License
 
 
