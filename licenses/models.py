eae1bbb4d77f0eb787172abaa49d4ab6534dc0ae
Andrew Miotke <andrewmiotke@gmail.com>
2021-02-23 15:09:14 -0800

    Search (#6)
    * Changed the order of which things appear in the license list
    
    * Search: Added a search bar to the navigation bar.
    
    - This search bar is currently not working
    - Comes from bootstrap for styling
    
    * Python formatting
    
    * Just stuff

--- licenses/models.py
+++ licenses/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
-
 from django.urls import reverse
 from django.db.models import Q
 
